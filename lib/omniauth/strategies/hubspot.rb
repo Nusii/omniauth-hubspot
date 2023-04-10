54cd66a8cd05523ad2f9905479f554e04ae63939
Matt Swanson <matt@arrows.to>
2023-04-10 10:37:31 -0400

    Allow passing full_host

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -89,7 +89,7 @@ module OmniAuth
       #private
 
       def callback_url
-        full_host + script_name + callback_path
+        options.full_host + script_name + callback_path
       end
     end
   end
